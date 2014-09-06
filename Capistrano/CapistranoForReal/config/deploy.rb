3ea012cc59e14616f143911c2dcea4681ee76ea0
tk1124 <th.tk.2012@gmail.com>
2014-09-06 23:53:39 +0900

    Move winrm_cmd to Capistrano module.

--- Capistrano/CapistranoForReal/config/deploy.rb
+++ Capistrano/CapistranoForReal/config/deploy.rb
@@ -16,49 +16,82 @@ Rake::Task[:deploy].clear
 
 set :application, 'SampleWebApp'
 
+
+#
+# Customize Capistrano
+#
+module ::Capistrano
+  class Configuration
+    module Actions
+      module WinRM
+        def winrm_cmd(cmd, options = {})
+          options = current_task_.options.merge(options)
+          
+          servers = find_servers_for_task(current_task, options)
+          raise Capistrano::NoMatchingServersError if servers.empty?
+          
+          servers.each do |server|
+            
+            winrm_exec(host, cmd) do |stdout, stderr|
+              info stdout if stdout
+              info stderr if stderr
+            end
+          end
+        end
+        
+        def winrm_exec(host, cmd)
+          hostname = host.hostname
+          username = host.user
+          endpoint = "http://#{hostname}:5985/wsman"
+          pass = get_password(hostname)
+          
+          std_o = "remote> #{cmd}"
+          std_e = ''
+          winrm = WinRM::WinRMWebService.new(endpoint, :plaintext, :user => username, :pass => pass, :basic_auth_only => true)
+          winrm.cmd(cmd) do |o, e|
+            std_o ? (std_o += o) : std_o = o
+            std_e = e
+          end
+
+          std_o += fetch :package_name
+          yield std_o, std_e
+        end
+        
+        
+        def get_password(hostname)
+          init_passwords unless fetch(:passwords)
+
+          fetch(:passwords)[hostname]
+        end
+
+        def init_passwords
+          filename = File.join(Dir.getwd, 'config/deploy', fetch(:password_file))
+          
+          File.open(filename) do |f|
+            passwords = {}
+            
+            f.each do |line|
+              pair = line.split()
+              
+              passwords[pair[0]] = pair[1]
+            end
+            
+            set :passwords, passwords
+          end
+        end
+      end
+    end
+  end
+end
+
+
+
+
 #
 # Methods
 #
-def init_passwords
-  filename = File.join(Dir.getwd, 'config/deploy', fetch(:password_file))
-  
-  File.open(filename) do |f|
-    passwords = {}
-    
-    f.each do |line|
-      pair = line.split()
-      
-      passwords[pair[0]] = pair[1]
-    end
-    
-    set :passwords, passwords
-  end
-end
 
 
-def get_password(hostname)
-  init_passwords unless fetch(:passwords)
-
-  fetch(:passwords)[hostname]
-end
-
-def winrm_cmd(host, cmd)
-  hostname = host.hostname
-  username = host.user
-  endpoint = "http://#{hostname}:5985/wsman"
-  pass = get_password(host.hostname)
-  
-  std_o = "remote> #{cmd}"
-  std_e = ''
-  winrm = WinRM::WinRMWebService.new(endpoint, :plaintext, :user => username, :pass => pass, :basic_auth_only => true)
-  winrm.cmd(cmd) do |o, e|
-    std_o ? (std_o += o) : std_o = o
-    std_e = e
-  end
-
-  std_o += fetch :package_name
-  yield std_o, std_e
-end
 
 
 def compress_files(src_path, dest_file_path)
@@ -66,7 +99,7 @@ def compress_files(src_path, dest_file_path)
   std_e = ''
   
   cmd = "7z a -r -tzip #{dest_file_path} #{src_path}"
-  winrm_cmd host, cmd do |stdout, stderr|
+  winrm_cmd cmd, {} do |stdout, stderr|
     std_o = stdout
     std_e = stderr
   end
